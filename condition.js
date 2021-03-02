8bbfead2f6b8d670f6ba5743db370ae7c927196f
shoaibahmadsakib <shoaiabahmadsakib@gmail.com>
2021-03-02 13:19:41 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,25 @@
+// var myCondition = 500;
+
+// if(myCondition ==500){
+//     console.log('this is true');
+// }
+// else{
+//     console.log('this is false');
+// }
+
+// console.log(myCondition == 500); 
+
+var myCondition;
+
+myCondition= prompt('enter a number');
+
+if(myCondition >= 80){
+   document.write(`you got gpa 5 `);
+}
+else if(myCondition >=70){
+   document.write('you got 4');
+}
+else{
+   document.write('fail');
+}
+
